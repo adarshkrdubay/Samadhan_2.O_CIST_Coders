c1aa642cdd63d02c599ff7289d580ba5d16ef201
Adarsh kumar <81159937+adarshkrdubay@users.noreply.github.com>
2025-08-24 02:59:32 +0530

    Day 1 answer updated by adarsh

--- Day1/adarshday1.js
+++ Day1/adarshday1.js
@@ -1,1 +1,17 @@
-console.log("Hello dev test")
+//Day 1 Print student details from an object
+class Student {
+  constructor(name, Sem, Enrol) {
+    this.name = name;
+    this.Sem = Sem;
+    this.Enrol = Enrol;
+  }
+
+  printDetails() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Sem: ${this.Sem}`);
+    console.log(`Enrol: ${this.Enrol}`);
+  }
+}
+
+const student1 = new Student("Adarsh", 5, "00000000000");
+student1.printDetails();
