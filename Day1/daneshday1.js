7a421bf89ff96f13cf7b38b2920670efba539aee
DaneshRoy <166588705+DaneshRoy@users.noreply.github.com>
2025-08-24 10:15:13 -0700

    Create daneshday1.js

---
+++ Day1/daneshday1.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "cessor",
+    rollNo: 101,
+    age: 20,
+    course: "Computer Science"
+};
+
+console.log("Student Details:");
+console.log(`Name: ${student.name}`);
+console.log(`Roll No: ${student.rollNo}`);
+console.log(`Age: ${student.age}`);
+console.log(`Course: ${student.course}`);
